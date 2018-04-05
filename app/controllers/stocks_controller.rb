cee900df8c8705943bcee7fa73d877a9fcc6606d
engstu <engstu2015@gmail.com>
2018-04-05 11:09:40 +0000

    Final Commit

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,10 +2,10 @@ class StocksController < ApplicationController
   
   def search
     if params[:stock].blank?
-      flash.now[:danger] = "You have entered an ampty search string"
+      flash.now[:danger] = "You have entered an empty search string"
     else
       @stock = Stock.new_from_lookup(params[:stock])
-      flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
+      flash.now[:danger] = "You have entered an incorrect symbol" if @stock.blank?
     end
     render partial: 'users/result'
   end
